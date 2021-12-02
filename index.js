d43d55b9f60d38c84f1f81e04293ec0dcef7fd3b
Salvador Oliveira <slsalvadorlucas@gmail.com>
2021-12-01 21:21:08 -0400

    Refactor forEach function

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 module.exports = {
   forEach(arr, fn) {
-    for (let i = 0; i < arr.length; i++) {
-      const value = arr[i];
-
-      fn(value, i);
+    for (let i in arr) {
+      fn(arr[i], i);
     }
   },
 };
