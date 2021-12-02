19568e34e9ede8fc473195d44f6b3281162e9cc1
Salvador Oliveira <slsalvadorlucas@gmail.com>
2021-12-01 21:50:11 -0400

    Add test to a map function

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { forEach } = require("./index.js");
+const { forEach, map } = require("./index.js");
 
 let sum = 0;
 
@@ -8,6 +8,28 @@ forEach([3, 6, 9], (value) => {
 
 if (sum !== 18) {
   throw new Error("Expected summing array to equal 18");
+} else {
+  console.log("test passed");
+}
+
+const result = map([3, 6, 9], (value) => {
+  return value * value;
+});
+
+if (result[0] !== 9) {
+  throw new Error(`Expected to find 9, but found ${result[0]}`);
+} else {
+  console.log("test passed");
+}
+
+if (result[1] !== 36) {
+  throw new Error(`Expected to find 36, but found ${result[1]}`);
+} else {
+  console.log("test passed");
+}
+
+if (result[2] !== 81) {
+  throw new Error(`Expected to find 81, but found ${result[2]}`);
 } else {
   console.log("test passed");
 }
